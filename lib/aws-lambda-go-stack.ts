34654bb345972170c74881df00e08035a9411155
Faddah Wolf <my_biz@me.com>
2025-03-07 11:19:52 -0800

    Added code for lambdaFunction, gateway, integration and testResource to work with lambda/main.go file.

--- lib/aws-lambda-go-stack.ts
+++ lib/aws-lambda-go-stack.ts
@@ -1,3 +1,4 @@
+import { intOptions } from './../node_modules/aws-cdk-lib/node_modules/yaml/types.d';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
@@ -7,12 +8,21 @@ export class AwsLambdaGoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const lambdaFunction = new lambda.Function(this, 'TutorialLambda', {
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'main',
+      runtime: lambda.Runtime.PROVIDED_AL2023,
+    });
     
-    
+    const gateway = new RestApi(this, 'TutorialGateway', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: ['*'],
+        allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      }
+    });
 
-
-
-
-
+    const integration = new LambdaIntegration(lambdaFunction);
+    const testResource = gateway.root.addResource('test');
+    testResource.addMethod('GET', integration, {});
   }
 }
