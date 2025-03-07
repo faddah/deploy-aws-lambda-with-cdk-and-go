c0e3526f59b15e6a20ff290d167aabe20a5195aa
Faddah Wolf <my_biz@me.com>
2025-03-07 09:44:30 -0800

    Deleted comments and added imorts of aws-cdk-lib/aws-lambda and aws-cdk-lib/aws-apigateway.

--- lib/aws-lambda-go-stack.ts
+++ lib/aws-lambda-go-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { RestApi, LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 
 export class AwsLambdaGoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    
+    
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsLambdaGoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+
+
+
   }
 }
